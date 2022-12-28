3425efc3277bfd6bd5b5ca7974f1d12b103f95aa
forrest <forrest@forrestbear.org>
2022-12-27 20:21:29 -0500

    added MobX and started handling the loading of activities

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Container } from 'semantic-ui-react';
 import { Activity } from '../models/activity';
 import NavBar from './NavBar';
@@ -7,6 +7,7 @@ import { v4 as uuid } from 'uuid';
 import agent from '../api/agent';
 import LoadingComponent from './LoadingComponent';
 import { useStore } from '../stores/store';
+import { observer } from 'mobx-react-lite';
 
 function App() {
 	const {activityStore} = useStore();
@@ -16,20 +17,11 @@ function App() {
         Activity | undefined
     >(undefined);
     const [editMode, setEditMode] = useState(false);
-    const [loading, setLoading] = useState(true);
     const [submitting, setSubmitting] = useState(false);
 
     useEffect(() => {
-        agent.Activities.list().then((response) => {
-            let activities: Activity[] = [];
-            response.forEach((activity) => {
-                activity.date = activity.date.split('T')[0];
-                activities.push(activity);
-            });
-            setActivities(activities);
-            setLoading(false);
-        });
-    }, []);
+		activityStore.loadActivities();
+    }, [activityStore]);
 
     function handleSelectActivity(id: string) {
         setSelectedActivity(activities.find((x) => x.id === id));
@@ -79,16 +71,14 @@ function App() {
         });
     }
 
-    if (loading) return <LoadingComponent content='Loading app...' />;
+    if (activityStore.loadingInitial) return <LoadingComponent content='Loading app...' />;
 
     return (
         <>
             <NavBar openForm={handleFormOpen} />
             <Container style={{ marginTop: '7em' }}>
-				<h2>{activityStore.title}</h2>
-
                 <ActivityDashboard
-                    activities={activities}
+                    activities={activityStore.activities}
                     selectedActivity={selectedActivity}
                     selectActivity={handleSelectActivity}
                     cancelSelectActivity={handleCancelSelectActivity}
@@ -104,4 +94,4 @@ function App() {
     );
 }
 
-export default App;
+export default observer(App);
